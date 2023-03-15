f499647bd6572a8d103f44b6a49cfbb7bb8d8cde
Trong Nghia Nguyen <nguyentrongnghiabl@gmail.com>
2023-03-15 13:20:05 +0700

    add files

---
+++ a.py
@@ -0,0 +1,11 @@
+print('Game Đấm Búa Bao')
+
+age = 14
+if age < 10:
+		print("con nit")
+elif age < 18:
+		print("tre trau")
+		if age >= 15 and age <=17:
+			print("sieu tre treu")
+else:
+		print("nguoi lon")
---
+++ usd.py
@@ -0,0 +1,17 @@
+#usd = input()
+#vnd = int(usd) * 22
+#print("vnd = " + str(vnd) + "k VND")
+
+a=1
+b=2
+
+print("a= " + str(a))
+print("b= " + str(b))
+
+temp=a # temp: bien tam
+a=b
+b=temp
+
+print("===")
+print("a= " + str(a))
+print("b= " + str(b))
